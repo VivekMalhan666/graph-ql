539da57d0583644cebebd9b2842f8ea82557d953
Vivek <amalhan43@gmail.com>
2023-05-11 15:48:31 +0530

    Rule 9
    Use Enums for Fields that can Only Take a Specific Set of Values

---
+++ graphql-design-pattern/typeDefs/09.Rule-9.js
@@ -0,0 +1,46 @@
+// Rule #9: Use Enums for Fields that can Only Take a Specific Set of Values
+const { ApolloServer, gql } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    image: Image!
+    description: String!
+  }
+
+  type Image {
+    id: ID!
+    url: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type GroupFeature {
+    feature: String!
+  }
+
+  enum GroupFeatureFields {
+    INCLINE_ENGINE
+    FOUR_CYLINDER_ENGINE
+    TWIN_CYLUNDER_ENGINE
+    RED_PAINT
+    BLACK_PAINT
+  }
+`;
