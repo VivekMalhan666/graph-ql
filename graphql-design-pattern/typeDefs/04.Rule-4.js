3264cab96cbdd4ccafcfb08256f4be254635f4f9
Vivek <amalhan43@gmail.com>
2023-05-11 15:42:04 +0530

    Rule 4
    - It's Easier to Add Fields than to Remove Them

---
+++ graphql-design-pattern/typeDefs/04.Rule-4.js
@@ -0,0 +1,29 @@
+// Rule #4: It's Easier to Add Fields than to Remove Them
+const { ApolloServer, gql } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    features: [GroupFeatures]
+    applyFeaturesSeparately: Boolean!
+    cars: [Car!]!
+    name: String!
+    imageId: ID!
+    bodyHtml: String!
+  }
+
+  type GroupFeature {
+    feature: String!
+  }
+`;
